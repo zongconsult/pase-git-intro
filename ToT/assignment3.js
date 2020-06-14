031abd86c3277d9f6da070819992334ed4231bb9
zongconsult <zongconsultinc@gmail.com>
2020-06-14 15:47:35 +0000

    Assignment 3

---
+++ ToT/assignment3.js
@@ -0,0 +1,8 @@
+// let x = 0;
+
+for (let x = 2; x < 81; x++) {
+  if (x % 2 == 0) {
+    console.log(x);
+  }
+}
+console.log("Done");
---
+++ ToT/js1.js
@@ -0,0 +1,8 @@
+let userAge = prompt("Please enter your age");
+const Permit_Age = 55;
+
+if (userAge < Permit_Age) {
+  console.log("You are too young to play this game");
+} else {
+  console.log("Welcome to ZumaDeluxe");
+}
