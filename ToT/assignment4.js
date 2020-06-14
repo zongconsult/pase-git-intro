d86fe9d989ac6d67e63b9dc58bd6b161e5145b6e
zongconsult <zongconsultinc@gmail.com>
2020-06-14 21:27:11 +0000

    I'm doing my assignment

---
+++ ToT/assignment4.js
@@ -0,0 +1,8 @@
+var myItem = prompt("Enter your item");
+if (myItem != null) {
+  console.log(myItem);
+
+  //   collect the Items form the person into an array into an array
+  var ItemList = [].push(myItem);
+  console.log(ItemList.ItemList);
+}
---
+++ ToT/objects.js
@@ -0,0 +1,3 @@
+let obj = { name: "Kofi", age: 25 };
+
+console.log(obj);
